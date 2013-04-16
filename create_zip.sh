aaccbf91a343f9b240a479f30273c8a9962264ec
Gunhild Griff With <griffen@gob.(none)>
2013-04-16 10:31:57 +0200

    update in sh-scripts

---
+++ create_zip.sh
@@ -0,0 +1,8 @@
+echo "##################"
+echo "# Delete old H5P #"
+echo "##################"
+rm picture_slider.h5p
+zip -r picture_slider.h5p *
+echo "################################"
+echo "# New Picture Slider is ready! #"
+echo "################################"
