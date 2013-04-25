3f0d314f6b003348f54f1673bf63e5bb23cb0d8a
Gunhild Griff With <griffen@gob.(none)>
2013-04-25 23:28:13 +0200

    symlink.sh and new pictures

---
+++ symlink.sh
@@ -0,0 +1,4 @@
+cd /var/www/drupal6/sites/default/files/h5p/libraries/  
+sudo rm -r H5P.PictureSlider-1.0
+sudo ln -s ~/dev/picture_slider/H5P.PictureSlider H5P.PictureSlider-1.0
+cd ~/dev/picture_slider/
